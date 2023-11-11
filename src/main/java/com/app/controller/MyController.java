6213c0b0f4f20a307e6182ab2b686d0057823a4e
vidyasagar-here <vidyasagar.here@gmail.com>
2023-11-11 14:20:37 +0530

    SRS-111123 - initial commit

--- src/main/java/com/app/controller/MyController.java
+++ src/main/java/com/app/controller/MyController.java
@@ -1,27 +1,24 @@
 package com.app.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.model.City;
-import com.app.service.ICityService;
-
 @RestController
 public class MyController  implements ErrorController{
 	
-	@Autowired
-	public ICityService cityService;
-	
-	@GetMapping(value = "/cities")
-	public List<City> getCities() {
 
-        List<City> cities = cityService.findAll();
+	@GetMapping(value = "/hello")
+	public String getInit() {
 
-        return cities;
+        return "Hi Sagar";
+    }
+
+    @GetMapping(value = "/bye")
+    public String getBye() {
+
+        return "Bye Sagar";
     }
 
 }
